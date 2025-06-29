07e3fa39c4b660b88db31f2c7771326a393c6bc5
KKNMAL003 <info19music@gmail.com>
2025-06-29 12:10:21 +0200

    Fix JSX syntax error by renaming index.ts to index.tsx

---
+++ index.tsx
@@ -0,0 +1,14 @@
+import '@expo/metro-runtime';
+import 'react-native-get-random-values';
+import './utils/global-error-handler';
+import './polyfills';
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
